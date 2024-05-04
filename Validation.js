0a9cbce671cb9746fdc6057fd1f7122cb6d9a945
rafaelcesar0 <rafaelcesar988@gmail.com>
2024-05-03 21:50:01 -0300

    Update methods and add jest

---
+++ Validation.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.validation = void 0;
+const validation = () => async (req, res, next) => {
+    console.log('teste');
+};
+exports.validation = validation;
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,10 +8,30 @@ router.get('/', (_, res) => {
   return res.send('Hello DEV!');
 });
 
+router.get(
+  '/cidades',
+  CidadesControler.getAllValidation,
+  CidadesControler.getAll
+);
 router.post(
   '/cidades',
   CidadesControler.createValidation,
   CidadesControler.create
+);
+router.get(
+  '/cidades/:id',
+  CidadesControler.getByIdValidation,
+  CidadesControler.getById
+);
+router.put(
+  '/cidades/:id',
+  CidadesControler.updateByIdValidation,
+  CidadesControler.updateById
+);
+router.delete(
+  '/cidades/:id',
+  CidadesControler.deleteByIdValidation,
+  CidadesControler.deleteById
 );
 
 export { router };
